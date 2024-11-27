81aa131b6a75b5567c0c0b817fcb36ce7e800a09
Aayush <ayushkarkee6@gmail.com>
2024-11-27 15:37:43 +0545

    Creating navlinks

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -4,9 +4,13 @@ const Nav = () => {
   return (
     <div className="fixed h-[12vh] z-[10] bg-[#1c1b22] w-full">
       <div className="flex items-center h-full justify-between mx-auto w-[95%] sm:w-[90%] xl:w-[80%]">
+        {/*Logo*/}
         <h1 className="font-semibold text-2xl">
           Aayush Karki <span className="text-4xl text-green-400">.</span>
         </h1>
+
+        {/* Nav Links */}
+        <div className="flex items-center gap-4"></div>
       </div>
     </div>
   );
