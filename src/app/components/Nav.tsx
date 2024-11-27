e03139f5920af1932bd7ec59a15ac57a44a5e807
Aayush <ayushkarkee6@gmail.com>
2024-11-27 15:32:45 +0545

    logo nav

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="fixed h-[12vh] z-[10] bg-[#1c1b22] w-full">
+      <div className="flex items-center h-full justify-between mx-auto w-[95%] sm:w-[90%] xl:w-[80%]">
+        <h1 className="font-semibold text-2xl">
+          Aayush Karki <span className="text-4xl text-green-400">.</span>
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
