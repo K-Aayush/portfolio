309bdc88897e8ad1256494729e8a97e50a0a8f53
Aayush <ayushkarkee6@gmail.com>
2024-11-27 16:51:08 +0545

    styled sidebar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -9,8 +9,9 @@ const Nav = () => {
     <div className="fixed h-[12vh] z-[10] bg-[#1c1b22] w-full">
       <div className="flex items-center h-full justify-between mx-auto w-[95%] sm:w-[90%] xl:w-[80%]">
         {/*Logo*/}
-        <h1 className="font-semibold text-2xl">
-          Aayush Karki <span className="text-4xl text-green-400">.</span>
+        <h1 className="font-semibold text-xl md:text-2xl">
+          Aayush Karki{" "}
+          <span className="text-2xl md:text-4xl text-green-500">.</span>
         </h1>
 
         <div className="flex items-center space-x-10">
@@ -25,7 +26,7 @@ const Nav = () => {
 
           {/*Button*/}
           <div className="flex items-center space-x-4">
-            <button className="px-4 py-2 bg-green-500 rounded-full font-semibold hover:bg-green-600 transition-all duration-200">
+            <button className="px-4 py-2 bg-green-500 rounded-3xl sm:font-semibold font-light hover:bg-green-600 transition-all duration-200">
               Hire Me
             </button>
 
@@ -37,12 +38,24 @@ const Nav = () => {
 
       {/*SideBar*/}
       <div className="fixed inset-0 bg-black opacity-70 w-full h-screen z-[50]"></div>
-      <div className="fixed top-0 left-0 w-[70%] sm:w-[50%] h-full bg-[#1c1b22] z-[100]">
-        <div className="flex justify-between items-center p-6">
-          <h1 className="font-medium text-xl">
+      <div className="fixed top-0 left-0 w-[70%] sm:w-[50%] h-full bg-[#1c1b22] z-[100] p-6">
+        {/*Logo and closeMenu*/}
+        <div className="flex justify-between items-center">
+          <h1 className="font-medium text-[20px]">
             Aayush Karki <span className="text-2xl text-green-500">.</span>
           </h1>
           <IoMdClose className="w-8 h-8 cursor-pointer text-white" />
+        </div>
+
+        {/*Sidebar Navlinks*/}
+        <div className="flex flex-col justify-center items-start space-y-6 py-10">
+          {navLinks.map((nav) => (
+            <Link key={nav.id} href={nav.url}>
+              <p className="nav_label text-[18px] border-white border-b-[1px] hover:border-none">
+                {nav.label}
+              </p>
+            </Link>
+          ))}
         </div>
       </div>
     </div>
