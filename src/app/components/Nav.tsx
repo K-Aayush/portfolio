4b0f3252f84f628c792d5c8d0bbda2266c4659bd
Aayush <ayushkarkee6@gmail.com>
2024-11-27 16:14:11 +0545

    addint hireme button in nav and styled it

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -11,13 +11,22 @@ const Nav = () => {
           Aayush Karki <span className="text-4xl text-green-400">.</span>
         </h1>
 
-        {/* Nav Links */}
         <div className="flex items-center space-x-10">
-          {navLinks.map((nav) => (
-            <Link key={nav.id} href={nav.url}>
-              <p className="nav_label">{nav.label}</p>
-            </Link>
-          ))}
+          {/* Nav Links */}
+          <div className="hidden md:flex items-center space-x-8">
+            {navLinks.map((nav) => (
+              <Link key={nav.id} href={nav.url}>
+                <p className="nav_label">{nav.label}</p>
+              </Link>
+            ))}
+          </div>
+
+          {/*Button*/}
+          <div className="flex items-center space-x-4">
+            <button className="px-4 py-2 bg-green-500 rounded-full font-semibold hover:bg-green-600 transition-all duration-200">
+              Hire Me
+            </button>
+          </div>
         </div>
       </div>
     </div>
