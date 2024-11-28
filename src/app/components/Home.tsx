82981cf2daeade9f8e0926d1fef5d0d16a7ffa2f
Aayush <ayushkarkee6@gmail.com>
2024-11-28 14:28:50 +0545

    created buttons

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import Resume from "./Resume";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <Resume />
     </div>
   );
 };
--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -20,9 +20,55 @@ const Resume = () => {
         );
       case "skills":
         return <div className="text-2xl font-bold">{skills.title}</div>;
+      default:
+        return null;
     }
   };
-  return <div>Resume</div>;
+
+  return (
+    <div className="py-16 bg-[#1c1b22]">
+      <div className="grid grid-cols-1 lg:grid-cols-3 mx-auto w-4/5 h-screen items-center gap-12">
+        {/*Left Content*/}
+        <div className="flex flex-col gap-6">
+          <button
+            onClick={() => setSelectedSection("aboutMe")}
+            className={`w-full rounded-xl py-4 ${
+              selectedSection === "aboutMe"
+                ? "bg-green-400 text-black"
+                : "bg-[#27272b]"
+            }`}
+          >
+            About me
+          </button>
+
+          <button
+            onClick={() => setSelectedSection("skills")}
+            className={`w-full rounded-xl py-4 ${
+              selectedSection === "skills"
+                ? "bg-green-400 text-black"
+                : "bg-[#27272b]"
+            }`}
+          >
+            Skills
+          </button>
+
+          <button
+            onClick={() => setSelectedSection("education")}
+            className={`w-full rounded-xl py-4 ${
+              selectedSection === "education"
+                ? "bg-green-400 text-black"
+                : "bg-[#27272b]"
+            }`}
+          >
+            Education
+          </button>
+        </div>
+
+        {/*Right Content*/}
+        <div className="col-span-2">hlo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
