efd5b4e611122984ec762e902575a10f59a25257
Aayush <ayushkarkee6@gmail.com>
2024-11-28 21:24:05 +0545

    wrote and styled title and desc

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const ContactForm = () => {
-  return <div>ContactForm</div>;
+  return (
+    <div className="bg-[#27272b] p-4 sm:p-10 rounded-lg">
+      <div className="flex flex-col gap-4">
+        <h1 className="bg-clip-text text-transparent bg-gradient-to-tr from-green-400 to-green-300 text-2xl md:text-3xl font-bold">
+          Let's Work Together
+        </h1>
+        <p className="text-white/60 text-xs md:text-sm">
+          Got a project or idea? Let's work together! Fill out the form to
+          connect.
+        </p>
+      </div>
+
+      {/* input fields */}
+      <div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default ContactForm;
