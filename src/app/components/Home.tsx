ae1dbb8e21a63b14d9bf5fc24157bb0f845ef21b
Aayush <ayushkarkee6@gmail.com>
2024-11-28 18:21:07 +0545

    creating base layour for project.tsx

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import Resume from "./Resume";
+import Project from "./Project";
 
 const Home = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
       <Resume />
+      <Project />
     </div>
   );
 };
--- src/app/components/Project.tsx
+++ src/app/components/Project.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div className="py-4 lg:py-16 bg-[#1c1b22] mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 mx-auto w-4/5 items-start gap-12"></div>
+    </div>
+  );
 };
 
 export default Project;
