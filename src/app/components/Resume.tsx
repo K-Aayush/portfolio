6fd7743dd8e5d0af4c4cf788f04d79aada90be32
Aayush <ayushkarkee6@gmail.com>
2024-11-28 13:59:51 +0545

    rendering content using switch

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -7,6 +7,21 @@ type section = "aboutMe" | "education" | "skills";
 
 const Resume = () => {
   const [selectedSection, setSelectedSection] = useState<section>("aboutMe");
+
+  const renderContent = () => {
+    switch (selectedSection) {
+      case "aboutMe":
+        return <div className="text-2xl font-bold">{aboutMe.title}</div>;
+      case "education":
+        return (
+          <div className="flex items-start justify-center text-2xl font-bold">
+            {education.icon} {education.title}
+          </div>
+        );
+      case "skills":
+        return <div className="text-2xl font-bold">{skills.title}</div>;
+    }
+  };
   return <div>Resume</div>;
 };
 
