917eeaed1ff71d1ef752fefa01fc85bed2b7200d
Aayush <ayushkarkee6@gmail.com>
2024-11-28 13:55:02 +0545

    importing necessary imports and using state

--- src/app/components/Resume.tsx
+++ src/app/components/Resume.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { education, aboutMe, skills } from "../constant/constant";
+
+type section = "aboutMe" | "education" | "skills";
 
 const Resume = () => {
+  const [selectedSection, setSelectedSection] = useState<section>("aboutMe");
   return <div>Resume</div>;
 };
 
