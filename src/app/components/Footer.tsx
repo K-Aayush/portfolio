4698eceb6f3f1d085d217775a9fe627621c78744
Aayush <ayushkarkee6@gmail.com>
2024-11-28 22:21:56 +0545

    created simple footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="py-16 bg-[#1c1b22]">Footer</div>;
+  return (
+    <div className="py-16 bg-[#1c1b22] w-full">
+      <div className="border-t-[1.5px] border-white/60"></div>
+      <div className="flex flex-col items-center justify-center w-[80%] mx-auto mt-5 gap-3">
+        <h1 className="sm:font-semibold font-normal text-lg sm:text-2xl">
+          Aayush Karki
+          <span className="text-2xl text-green-500"> .</span>
+        </h1>
+
+        <p className="text-sm text-white/60">© 2024 All Rights Reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -11,7 +11,7 @@ const Nav = () => {
 
   return (
     <div className="fixed h-[12vh] z-[10] bg-[#1c1b22] w-full">
-      <div className="flex items-center h-full justify-between mx-auto w-[95%] sm:w-[90%] xl:w-[80%]">
+      <div className="flex items-center h-full justify-between mx-auto w-[95%] sm:w-[90%] lg:w-[80%]">
         {/*Logo*/}
         <h1 className="sm:font-semibold font-normal text-lg sm:text-2xl">
           Aayush Karki
