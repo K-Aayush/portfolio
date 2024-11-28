a033eee07bb2ab5a2f3ab3deac50b1f237ce6cbc
Aayush <ayushkarkee6@gmail.com>
2024-11-28 21:11:36 +0545

    created contactform and contactinfo files

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import ContactForm from "./ContactForm";
+import ContactInfo from "./ContactInfo";
 
 const Contact = () => {
   return (
     <div className="py-4 lg:py-16 bg-[#1c1b22]">
       <div className="grid grid-cols-1 lg:grid-cols-2 w-[80%] mx-auto items-center gap-12">
         {/* Contact Form */}
-        <div>hi</div>
+        <div>
+          <ContactForm />
+        </div>
 
         {/* Contact Info */}
-        <div>hlo</div>
+        <div>
+          <ContactInfo />
+        </div>
       </div>
     </div>
   );
